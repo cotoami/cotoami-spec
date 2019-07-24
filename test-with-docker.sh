659ceb7ebffc01b98e2132d15e7f868d970cbd63
Daisuke Morita <daisuke.marubinotto@gmail.com>
2019-07-24 19:22:21 +0900

    Add a Cotoami spec

--- test-with-docker.sh
+++ test-with-docker.sh
@@ -6,6 +6,17 @@ else
   DOCKER_HOST_IP="127.0.0.1"
 fi
 
+
+# Cotoami
+export COMPOSE_PROJECT_NAME=cotoami
+export COTOAMI_VERSION="v0.23.0"
+export COTOAMI_HOST=$DOCKER_HOST_IP
+
+wget -q https://raw.githubusercontent.com/cotoami/cotoami/$COTOAMI_VERSION/launch/docker-compose.yml -O docker-compose.yml
+
+docker-compose up -d
+
+
 # Selenium Server
 echo
 echo "# Running selenium server..."
@@ -19,13 +30,16 @@ while ! nc -z $DOCKER_HOST_IP 4444; do
   sleep 1s
 done
 
+
 # Run specs
 stack --docker test
+
 
 # Make sure to tear down the backend containers
 function tear_down_containers() {
   echo
   echo "# Tearing down containers..."
   docker stop $CID_SELENIUM && docker rm $CID_SELENIUM
+  docker-compose down -v
 }
 trap tear_down_containers 0 1 2 3 15
